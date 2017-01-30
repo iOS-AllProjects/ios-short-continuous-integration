26c4b412f6ab35b70e3746f71c5387f454696071
Etjen123 <etjenymeraj@Etjens-MacBook-Pro-2.local>
2017-01-30 16:16:44 +0100

    add toRoman unit tests

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -2,8 +2,8 @@
 //  RomanNumeralCalculatorUITests.swift
 //  RomanNumeralCalculatorUITests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Etjen Ymeraj on 1/30/17.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
